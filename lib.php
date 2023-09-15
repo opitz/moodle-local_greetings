29812d0b789c7695067ce66c71725fd0e3cc6c69
Matthias Opitz <m.opitz@ucl.ac.uk>
2023-09-15 16:05:51 +0100

    Login and user input sanity check

--- lib.php
+++ lib.php
@@ -53,20 +53,30 @@ function local_greetings_get_greeting($user) {
  * @param navigation_node $frontpage Node representing the front page in the navigation tree.
  */
 function local_greetings_extend_navigation_frontpage(navigation_node $frontpage) {
-    $frontpage->add(
-        get_string('pluginname', 'local_greetings'),
-        new moodle_url('/local/greetings/index.php'),
-        navigation_node::TYPE_CUSTOM,
-    );
+    if (!isguestuser()) {
+        $frontpage->add(
+            get_string('pluginname', 'local_greetings'),
+            new moodle_url('/local/greetings/index.php'),
+            navigation_node::TYPE_CUSTOM,
+        );
+    }
 }
 
-function local_greetings_extend_navigation(global_navigation $root)
-{
-    $node = navigation_node::create(
-        get_string('pluginname', 'local_greetings'),
-        new moodle_url('/local/greetings/index.php')
-    );
+/**
+ * Insert a global navigation link
+ *
+ * @param global_navigation $root
+ * @return void
+ * @throws coding_exception
+ */
+function local_greetings_extend_navigation(global_navigation $root) {
+    if (!isguestuser()) {
+        $node = navigation_node::create(
+            get_string('pluginname', 'local_greetings'),
+            new moodle_url('/local/greetings/index.php')
+        );
 
-    $node->showinflatnavigation = true;
-    $root->add_node($node);
+        $node->showinflatnavigation = true;
+        $root->add_node($node);
+    }
 }
